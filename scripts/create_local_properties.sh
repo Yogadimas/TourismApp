454fc253d4dcf858e750c94b3dfc0819e574708a
Yogadimas <yogadimaspambudi@gmail.com>
2025-03-09 22:58:58 +0700

    CircleCI

--- scripts/create_local_properties.sh
+++ scripts/create_local_properties.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-echo "Creating local.properties file..."
+echo "Writing local.properties..."
 
 cat <<EOL > local.properties
-sdk.dir=$ANDROID_SDK_ROOT
-apiKeyNvd=$API_KEY_NVD
-baseUrlApi=$BASE_URL_API
-baseUrlHost=$BASE_URL_HOST
-certSha256_1=$CERT_SHA_256_1
-certSha256_2=$CERT_SHA_256_2
-certSha256_3=$CERT_SHA_256_3
+API_KEY_NVD=${API_KEY_NVD}
+BASE_URL_API=${BASE_URL_API}
+BASE_URL_HOST=${BASE_URL_HOST}
+CERT_SHA_256_1=${CERT_SHA_256_1}
+CERT_SHA_256_2=${CERT_SHA_256_2}
+CERT_SHA_256_3=${CERT_SHA_256_3}
 EOL
 
-echo "local.properties created successfully!"
+echo "local.properties created:"
+cat local.properties
